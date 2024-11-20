a89fb0f59474ca09a3e458844804335bf514e850
Yevhenii Dushkevych <dushkevich1996@gmail.com>
2024-11-20 16:19:34 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largImg = document.getElementById('largeImg');
+const smallList = document.getElementById('thumbs');
+
+smallList.addEventListener('click', (ev) => {
+  event.preventDefault();
+
+  if (ev.target) {
+    const currentArt = event.target.getAttribute('src');
+
+    largImg.setAttribute('src', currentArt);
+  }
+});
